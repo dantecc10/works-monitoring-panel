8a8e3c19636ccafcb88ddeb550a9938c9d972267
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-07-09 22:36:40 -0600

    test dir

--- php scripts/connection.php
+++ php scripts/connection.php
@@ -1,3 +1,3 @@
 <?php
-
-	$connection = new mysqli('localhost', 'works-mp', $sql_secret, 'works-monitoring-panel');
+echo ("Ruta: " . dirname(dirname(__FILE__), 10));
+//$connection = new mysqli('localhost', 'works-mp', $sql_secret, 'works-monitoring-panel');
