e691ee34da59e408575c7f0ed4dbaf674b2f478a
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-07-21 00:06:09 -0600

    copy

---
+++ phpscripts/functions.php
@@ -0,0 +1,15 @@
+<?php
+function check_internet_connection()
+{
+    $conn = @fsockopen('obras.bmdesign.tech', 80);
+    if ($conn) {
+        fclose($conn);
+        return true; // Conexión exitosa
+    } else {
+        return false; // Sin conexión
+    }
+    //echo (check_internet_connection()) ? "Hay conexión a Internet." : "No hay conexión a Internet.";
+}
+function show_projects($id){
+    include "connection.php";
+}
