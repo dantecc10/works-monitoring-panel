47fee33311176703ef26f1d1c8927ebd5f3629e3
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-08-24 16:30:10 -0600

    updating code

--- php-scripts/back-debugger.php
+++ php-scripts/back-debugger.php
@@ -1,3 +1,12 @@
 <?php
 include "connection.php";
 echo ($connection->connect_error) ? ("Connection failed: " . $connection->connect_error) : "Connected successfully to database!";
+
+function fetch_team_data($connection, $team_id)
+{
+    $query = "SELECT * FROM `teams` WHERE (`id_team` = $team_id)";
+    $result = $connection->query($query);
+    return $result->fetch_assoc();
+}
+
+print_r (fetch_team_data($connection, 1));
