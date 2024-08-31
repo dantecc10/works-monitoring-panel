b1fffef23e5976498af1f97e6f0376f5f4d33478
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-08-31 12:32:37 -0600

    adding query

--- php-scripts/functions.php
+++ php-scripts/functions.php
@@ -54,3 +54,82 @@ function flag_replacer($text, $flag, $data_array, $indexes_array)
         return null;
     }
 }
+
+function data_fetcher($connection, $element_id, $type)
+{
+    $only_row = true;
+    switch ($type) {
+        case 'team':
+            $query = "SELECT 
+                            `t`.*, 
+                            CASE 
+                                WHEN `t`.`work_area_team` = 1 THEN 'Eléctrico'
+                                WHEN `t`.`work_area_team` = 2 THEN 'Plomería'
+                                WHEN `t`.`work_area_team` = 3 THEN 'Civil'
+                                ELSE 'Área Desconocida'
+                            END AS `team_area`,
+                            CONCAT(u.name_user, ' ', u.last_names_user) AS leader_team
+                        FROM 
+                            `teams` `t`
+                        JOIN 
+                            `users` `u` ON `t`.`id_user_team` = `u`.`id_user`
+                        WHERE 
+                            `t`.`id_team` = ?;";
+            break;
+        case 'user':
+            $query = "SELECT 
+            `u`.`id_user`,
+            `u`.`name_user`, 
+            `u`.`last_names_user`, 
+            `u`.`email_user`,
+            `u`.`role_user`,
+            `u`.`icon_user`, 
+            CASE 
+                WHEN `u`.`role_user` = 1 THEN 'Propietario'
+                WHEN `u`.`role_user` = 2 THEN 'Equipo de Trabajo'
+                WHEN `u`.`role_user` = 3 THEN 'Administrador'
+                ELSE 'Rol Desconocido'
+            END AS `job_user`
+                FROM `users` `u` WHERE `u`.`id_user`= ?"; // Lógica para 'user'
+            break;
+        case 'task':
+            $query = "SELECT `t`.*, `p`.`name_project` AS `project_name`,
+                            `tm`.`company_team` AS `team_name`
+                        FROM `tasks` `t` JOIN 
+                            `projects` `p` ON `t`.`id_project_task` = `p`.`id_project`
+                        JOIN 
+                            `teams` `tm` ON `t`.`id_team_task` = `tm`.`id_team`
+                        WHERE 
+                            `t`.`id_task` = ?;"; // Lógica para 'task'
+            break;
+        case "teams":
+            $query = "SELECT DISTINCT `tm`.`id_team`
+                        FROM `tasks` `t`
+                        JOIN 
+                            `teams` `tm` ON `t`.`id_team_task` = `tm`.`id_team`
+                        WHERE `t`.`id_project_task` = ? ORDER BY 
+                            `tm`.`id_team` ASC;";
+            $only_row = false;
+            break;
+        case "project-teams":
+            $query = "SELECT DISTINCT `id_team_task` FROM `tasks` WHERE (`id_project_task` = ?);";
+            break;
+        default:
+            return false;
+    }
+
+    $stmt = $connection->prepare($query);
+    $stmt->bind_param("i", $element_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if (!$only_row) {
+        $data = [];
+        while ($row = $result->fetch_assoc()) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+
+    return ($result->num_rows > 0) ? $result->fetch_assoc() : false;
+}
