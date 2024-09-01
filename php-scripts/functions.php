5ae5bc61f58025b3bbd995294bd838289daf6530
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-09-01 15:46:51 -0600

    updating code

--- php-scripts/functions.php
+++ php-scripts/functions.php
@@ -186,8 +186,8 @@ function detail_build_teams($project)
             $team = $teams_data[$i];
             $n = $i + 1;
             $temp_field = ("DATA_" . strtoupper($fields[$i]));
-            $team_dom = str_replace("item-n", "item-$n", $team_dom);
-            $team_dom = preg_replace("/$temp_field/", $team[$fields[$i]], $detailed_team_dom, 1);
+            $team_dom = str_replace("item-n", "item-$n", $detailed_team_dom);
+            $team_dom = preg_replace("/$temp_field/", $team[$fields[$i]], $team_dom, 1);
             $teams_dom_output .= $team_dom;
         }
         return str_replace("INSERT_TEAMS_DOM", $teams_dom_output, $detail_teams_dom);
