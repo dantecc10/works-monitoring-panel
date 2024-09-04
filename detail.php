83161e595d250aac3bf7e609a8d3683a7708403f
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-09-04 04:11:19 -0600

    updating backend code

--- detail.php
+++ detail.php
@@ -35,7 +35,7 @@ $project_data = data_fetcher($connection, $_GET['id'], "project");
     <div id="wrapper">
         <nav class="navbar align-items-start sidebar sidebar-dark accordion main-gradient p-0 navbar-dark">
             <div class="container-fluid d-flex flex-column p-0 text-center"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
-                    <div class="sidebar-brand-icon" style="height: inherit;"><img src="<?php echo ($project_data['icon_user']); ?>" style="height: inherit;"></div>
+                    <div class="sidebar-brand-icon" style="height: inherit;"><img src="assets/img/logo.png" style="height: inherit;"></div>
                     <div class="sidebar-brand-text mx-3"><span>Obras - BMD</span></div>
                 </a>
                 <hr class="sidebar-divider my-0">
@@ -133,7 +133,7 @@ $project_data = data_fetcher($connection, $_GET['id'], "project");
                             </li>
                             <div class="d-none d-sm-block topbar-divider"></div>
                             <li class="nav-item dropdown no-arrow">
-                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small">Dante (Propietario)</span><img class="border rounded-circle img-profile" src="assets/img/avatars/default-avatar.png"></a>
+                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small">Dante (Propietario)</span><img class="border rounded-circle img-profile" src="<?php echo ($project_data['icon_user']); ?>"></a>
                                     <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Perfil</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Configuración</a><a class="dropdown-item" href="#"><i class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Actividad</a>
                                         <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Cerrar sesión</a>
                                     </div>
