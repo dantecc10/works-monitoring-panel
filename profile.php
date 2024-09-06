696fae932d97d5b812f7b476deab5510438f6969
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-09-06 02:31:12 -0600

    updating code

--- profile.php
+++ profile.php
@@ -153,8 +153,10 @@ include 'php-scripts/connection.php';
                     <div class="row mb-3">
                         <div class="col-lg-4">
                             <div class="card mb-3">
-                                <div class="card-body text-center shadow"><img class="rounded-circle mb-3 mt-4" src="<?php echo (isset($_SESSION['icon_user'])) ? $_SESSION['icon_user']  : "assets/img/avatars/default-avatar.png"; ?>" width="160" height="160">
-                                    <div class="mb-3"><button class="btn btn-sm bg-color-5 color-2 fw-bolder fs-6 rounded-5" type="button">Cambiar imagen de perfil</button></div>
+                                <div class="card-body text-center shadow"><img class="rounded-circle mb-3 mt-4" id="avatar-img" src="<?php echo (isset($_SESSION['icon_user'])) ? $_SESSION['icon_user']  : "assets/img/avatars/default-avatar.png"; ?>" width="160" height="160">
+                                    <form id="form-img" method="post" action="php-scripts/tasks.php?task=update-user-img">
+                                        <div class="mb-3"><button class="btn btn-sm bg-color-5 color-2 fw-bolder fs-6 rounded-5" id="change-img-button" type="button" onclick="javascript:img_picker(&#39;pick&#39;);">Cambiar imagen de perfil</button><input class="form-control visu" type="file" id="img-form" name="img" required="" onchange="javascript:img_picker(&#39;picked&#39;);" accept="image/*"></div>
+                                    </form>
                                 </div>
                             </div>
                             <div class="card shadow mb-4 visually-hidden">
