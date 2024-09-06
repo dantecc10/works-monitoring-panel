56a9a8e1231b37c443bb8fcec60b058ba78fb683
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-09-06 01:22:52 -0600

    updating code

--- php-scripts/tasks.php
+++ php-scripts/tasks.php
@@ -13,9 +13,26 @@ switch ($_GET['task']) {
         $name = $_POST['name'];
         $last_names = $_POST['last_names'];
         $email = $_POST['email'];
-        
+
+        $sql = "UPDATE `users` SET `name_user` = ?, `last_names_user` = ?, `email_user` = ? WHERE `id_user` = ?";
+        if ($stmt = $connection->prepare($sql)) {
+            $stmt->bind_param("sssi", $name, $last_names, $email, $id);
+            if ($stmt->execute()) {
+                $_SESSION['name_user'] = $name;
+                $_SESSION['last_names_user'] = $last_names;
+                $_SESSION['email_user'] = $email;
+                $_SESSION['pending_msg']['type'] = 'success';
+                $_SESSION['pending_msg']['text'] = 'Datos actualizados correctamente.';
+                header('Location: ../profile.php?success=updated');
+            } else {
+                header('Location: ../profile.php?error=update');
+                exit;
+            }
+            $stmt->close();
+        }
+
         break;
-    
+
     default:
         # code...
         break;
