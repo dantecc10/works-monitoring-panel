1a59d7f1fe11f7273ae6cea0afb62d50f323aede
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-09-20 14:47:07 -0600

    updating code

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
 	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
 	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-	  return new bootstrap.Tooltip(tooltipTriggerEl);
+		return new bootstrap.Tooltip(tooltipTriggerEl);
 	})
 
 	var charts = document.querySelectorAll('[data-bss-chart]');
