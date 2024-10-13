e55aca331975c953df1f3e340e561cbd4f32e094
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-10-13 01:15:01 -0600

    updating code

--- detail.php
+++ detail.php
@@ -174,7 +174,9 @@ $js_imgs_array .= "];</script>";
                             ?>
 
                             <li class="nav-item dropdown no-arrow">
-                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="d-none d-lg-inline me-2 text-gray-600 small"><?php echo ($_SESSION['name_user']); ?> (<?php echo ($_SESSION['type_user']);?>)</span><img class="border rounded-circle img-profile" src="<?php echo ($project_data['icon_user']); ?>"></a>
+                                <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">
+                                    <span class="d-none d-lg-inline me-2 text-gray-600 small"><?php echo ($_SESSION['name_user']); ?> (<?php echo ($_SESSION['type_user']);?>)</span>
+                                    <img class="border rounded-circle img-profile" src="<?php echo ($project_data['icon_user']); ?>"></a>
                                     <div class="dropdown-menu shadow dropdown-menu-end animated--grow-in"><a class="dropdown-item" href="#"><i class="fas fa-user fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Perfil</a><a class="dropdown-item" href="#"><i class="fas fa-cogs fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Configuración</a><a class="dropdown-item" href="#"><i class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Actividad</a>
                                         <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Cerrar sesión</a>
                                     </div>
